3fa13addb8ff67f61642599f1015f811507b0570
Hans Yulian <hansyulian@windowslive.com>
2024-10-24 11:40:57 +0700

    mantine setup

---
+++ src/pages/_app.tsx
@@ -0,0 +1,19 @@
+// Import styles of packages that you've installed.
+// All packages except `@mantine/hooks` require styles imports
+import "@mantine/core/styles.css";
+
+import type { AppProps } from "next/app";
+import { createTheme, MantineProvider } from "@mantine/core";
+
+const theme = createTheme({
+  /** Put your mantine theme override here */
+});
+
+export default function App({ Component, pageProps }: AppProps) {
+  console.log("mantine dev setup");
+  return (
+    <MantineProvider theme={theme}>
+      <Component {...pageProps} />
+    </MantineProvider>
+  );
+}
